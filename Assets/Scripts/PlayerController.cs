bf162a7de3b07935ceacc2feb5833e32887af652
hansow <nachoagus@live.com.ar>
2019-04-24 17:29:32 -0300

    Push inicial con movimiento del tank y ui

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Joystick movementJoystick;
+    [SerializeField] Joystick aimJoystick;
+
+    private Movement movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        movement.Move(movementJoystick.Direction);
+        Debug.Log(movementJoystick.Direction.magnitude);
+    }
+}
