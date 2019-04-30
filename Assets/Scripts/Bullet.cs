faab3e7130a33a74fc4a583826291267db570647
hansow <nachoagus@live.com.ar>
2019-04-30 17:04:17 -0300

    El tanque dispara y apunta

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    private string myShooterLayer;
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    public void Spawn(string shooter)
+    {
+        myShooterLayer = shooter;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == myShooterLayer) return;
+
+        
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+         
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Joystick movementJoystick;
     [SerializeField] Joystick aimJoystick;
+    [SerializeField] Button fireButton;
+    [SerializeField] Image fireImage;
 
     private Movement movement;
+    private Attack attack;
 
     private void Awake()
     {
         movement = GetComponent<Movement>();
+        attack = GetComponent<Attack>();
+        fireButton.onClick.AddListener(attack.Shoot);
     }
 
     private void Update()
     {
+        fireImage.fillAmount = attack.RemainingCooldown / attack.AttackSpeed;
         movement.Move(movementJoystick.Direction);
-        Debug.Log(movementJoystick.Direction.magnitude);
+        attack.Aim(aimJoystick.Direction);
     }
 }
